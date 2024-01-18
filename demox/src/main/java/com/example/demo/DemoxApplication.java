f111ba60ba24d00de75ab56f33106ea8edb527a0
sypark <sypark@LAPTOP-HG4T6B9M>
2024-01-18 17:11:26 +0900

    1

---
+++ demox/src/main/java/com/example/demo/DemoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoxApplication.class, args);
+	}
+
+}
